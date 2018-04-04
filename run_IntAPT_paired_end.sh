d8fb16d5dfc4c14991797992a0aaa4dddbbba3bd
Xu Shi <henryxushi@gmail.com>
2018-04-04 16:51:35 -0400

    v1.1

--- run_IntAPT_paired_end.sh
+++ run_IntAPT_paired_end.sh
@@ -1,11 +1,13 @@
 BAMLIST=/home/cbil/Henry/IntAPT_demo/demobamlist.txt
 INTAPT_PATH=/home/cbil/Henry/IntAPT_v1.1
 OUTPUT=/home/cbil/Henry/IntAPT_demo/demo_paired_end
-NUM_OF_PROCESSES=1
+NUM_OF_PROCESSES=5
 
 chmod 777 $INTAPT_PATH/IntAPT
 chmod 777 $INTAPT_PATH/tools/samtools
 chmod 777 $INTAPT_PATH/tools/processsamS
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INTAPT_PATH/lib
+#please add the boost library and bamtools library to system lib using the following command
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:
+
 $INTAPT_PATH/IntAPT -b $BAMLIST -o $OUTPUT -r p -p $NUM_OF_PROCESSES --IntAPTpath $INTAPT_PATH/tools
